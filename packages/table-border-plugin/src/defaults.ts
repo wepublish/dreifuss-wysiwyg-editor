5a96b69b06994bf51b224dca00aa72c9ac3bb13b
HanaAyman <hanaayman1997@gmail.com>
2021-07-05 16:32:53 +0200

    fix

--- packages/table-border-plugin/src/defaults.ts
+++ packages/table-border-plugin/src/defaults.ts
@@ -1,7 +1,7 @@
 import {SlatePluginOptions} from '@udecode/slate-plugins-core'
 
-export const ELEMENT_BORDER_COLOR = 'color'
+export const ELEMENT_BORDER_COLOR = 'borderColor'
 
 export const DEFAULTS_BORDER_COLOR: Partial<SlatePluginOptions> = {
-  getNodeProps: ({element}) => ({color: element?.color})
+  getNodeProps: ({element}) => ({borderColor: element?.borderColor})
 }
