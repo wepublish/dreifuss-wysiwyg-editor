b08c0ca9ea84b64ca9563454764d77c702ffa0bc
Nicolas Roos <nicolas.k.roos@gmail.com>
2021-07-08 13:24:55 +0200

    chore(app): i hope this works now

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    plugins: [],
+    presets: [
+        ['@babel/preset-react', { runtime: 'automatic' }],
+        '@babel/preset-typescript'
+    ],
+    sourceType: 'unambiguous',
+};
