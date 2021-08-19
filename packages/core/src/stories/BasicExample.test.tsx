fce34d234f9d6609843db77532348a439908f08a
HanaAyman <hanaayman1997@gmail.com>
2021-08-19 12:39:34 +0200

    fix

--- packages/core/src/stories/BasicExample.test.tsx
+++ packages/core/src/stories/BasicExample.test.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-// import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom/extend-expect';
 
 import { Primary } from './BasicExample.stories';
 
-it('renders the button in the primary state', () => {
+test('renders the button in the primary state', () => {
     render(<Primary {...Primary.args} />);
-    expect(screen.getByRole('button')).toHaveTextContent('Primary');
+    expect(screen.getByRole('button')).toHaveTextContent('Button');
 });
