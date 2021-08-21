dab6eae23a7572fcecc3d2af9980027a6de9c49c
Bishoy <bishoymelekwadie@gmail.com>
2021-08-21 14:30:43 +0200

    cleanup

--- packages/core/__tests__/DreifussWysiwygEditor-test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import renderer from 'react-test-renderer'
-import DreifussWysiwygEditor from '../src/DreifussWysiwygEditor'
-
-test('Props is displayed', () => {
-  const component = renderer.create(<DreifussWysiwygEditor title="Hello" />)
-  const instance = component.root
-  expect(instance.findByProps({className: 'title'}).children).toEqual(['Hello'])
-})
