797b79205d050896ff8bb6f1ccbe284c860e0672
bishoy melek <bishoymelekwadie@gmail.com>
2021-09-01 03:47:12 +0200

    twin.macro - fixes in tableElement

--- packages/table-ui/src/TableElement/TableElement.styles.ts
+++ packages/table-ui/src/TableElement/TableElement.styles.ts
@@ -1,10 +1,11 @@
-import {createStyles, StyledElementProps} from '@udecode/plate-styled-components'
-import tw from 'twin.macro'
+// import {createStyles, StyledElementProps} from '@udecode/plate-styled-components'
+// import tw from 'twin.macro'
 
-export const getTableElementStyles = (props: StyledElementProps) =>
-  createStyles(
-    {prefixClassNames: 'TableElement', ...props},
-    {
-      root: tw`w-full my-2.5 mx-0 border-collapse`
-    }
-  )
+// TODO: should uncomment and use this.
+// export const getTableElementStyles = (props: StyledElementProps) =>
+//   createStyles(
+//     {prefixClassNames: 'TableElement', ...props},
+//     {
+//       // root: tw`w-full my-2.5 mx-0 border-collapse`
+//     }
+//   )
--- packages/table-ui/src/TableElement/TableElement.tsx
+++ packages/table-ui/src/TableElement/TableElement.tsx
@@ -1,17 +1,20 @@
 import * as React from 'react'
 import {StyledElementProps} from '@udecode/plate-styled-components'
-import {getTableElementStyles} from './TableElement.styles'
+// import {getTableElementStyles} from './TableElement.styles'
 
 export const TableElement = (props: StyledElementProps) => {
   const {attributes, children, nodeProps} = props
 
-  const {root} = getTableElementStyles(props)
+  // const {root} = getTableElementStyles(props)
 
   return (
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    // TODO: check this
-    <table {...attributes} css={root.css} className={root.className} {...nodeProps}>
+    <table
+      {...attributes}
+      style={{margin: '10px 0', borderCollapse: 'collapse', width: '100%'}}
+      // TODO: let the .styles file work and use the commented following lines
+      // css={root.css}
+      // className={root.className}
+      {...nodeProps}>
       <tbody>{children}</tbody>
     </table>
   )
