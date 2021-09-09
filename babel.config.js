248d517b74f88abdd5ca3f30a07e25338b862692
bishoy melek <bishoymelekwadie@gmail.com>
2021-09-09 23:48:48 +0200

    enhancements for twin styled components

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  plugins: ['macros'],
+  plugins: ['babel-plugin-twin', 'babel-plugin-macros', ['styled-components', {ssr: true}]],
   presets: [['@babel/preset-react', {runtime: 'automatic'}], '@babel/preset-typescript'],
   sourceType: 'unambiguous'
 }
