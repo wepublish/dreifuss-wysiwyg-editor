b82739833603e0dfb701805ef8e7cf54fba3e013
bishoy melek <bishoymelekwadie@gmail.com>
2021-09-09 17:35:46 +0200

    fixes in table-ui

--- packages/table-ui/src/TableElement/TableElement.tsx
+++ packages/table-ui/src/TableElement/TableElement.tsx
@@ -1,20 +1,17 @@
 import * as React from 'react'
 import {StyledElementProps} from '@udecode/plate-styled-components'
-// import {getTableElementStyles} from './TableElement.styles'
+import {getTableElementStyles} from './TableElement.styles'
 
 export const TableElement = (props: StyledElementProps) => {
   const {attributes, children, nodeProps} = props
 
-  // const {root} = getTableElementStyles(props)
+  const {root} = getTableElementStyles(props)
 
   return (
-    <table
-      {...attributes}
-      style={{margin: '10px 0', borderCollapse: 'collapse', width: '100%'}}
-      // TODO: let the .styles file work and use the commented following lines
-      // css={root.css}
-      // className={root.className}
-      {...nodeProps}>
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    // TODO: fix css attr
+    <table {...attributes} css={root.css} className={root.className} {...nodeProps}>
       <tbody>{children}</tbody>
     </table>
   )
