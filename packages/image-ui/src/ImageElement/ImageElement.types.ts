ff1e65e907f68fe01c00848ac1653a97190490ab
bishoy melek <bishoymelekwadie@gmail.com>
2021-09-27 11:40:19 +0200

    updated image-ui plugin with resizing option

--- packages/image-ui/src/ImageElement/ImageElement.types.ts
+++ packages/image-ui/src/ImageElement/ImageElement.types.ts
@@ -1,5 +1,6 @@
 import {ImageNodeData} from '@dreifuss-wysiwyg-editor/image'
 import {StyledElementProps} from '@udecode/plate-styled-components'
+import {ResizableProps} from 're-resizable'
 import {CSSProp} from 'styled-components'
 
 export interface ImageElementStyleProps extends ImageElementProps {
@@ -8,7 +9,40 @@ export interface ImageElementStyleProps extends ImageElementProps {
 }
 
 export interface ImageElementStyles {
+  resizable: CSSProp
+  figure: CSSProp
   img: CSSProp
+  figcaption: CSSProp
+  caption: CSSProp
+  handle: CSSProp
+  handleLeft: CSSProp
+  handleRight: CSSProp
 }
 
-export type ImageElementProps = StyledElementProps<ImageNodeData, ImageElementStyles>
+export interface ImageElementProps extends StyledElementProps<ImageNodeData, ImageElementStyles> {
+  resizableProps?: ResizableProps
+
+  /**
+   * Image alignment.
+   */
+  align?: 'left' | 'center' | 'right'
+
+  caption?: {
+    disabled?: boolean
+
+    /**
+     * Caption alignment.
+     */
+    align?: 'left' | 'center' | 'right'
+
+    /**
+     * Caption placeholder.
+     */
+    placeholder?: string
+  }
+
+  /**
+   * Whether the image is draggable.
+   */
+  draggable?: boolean
+}
