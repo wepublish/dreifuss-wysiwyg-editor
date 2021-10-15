bd8d8237bb61ddfd16dfdffee724e90e13735ec9
bishoy melek <bishoymelekwadie@gmail.com>
2021-10-15 21:25:44 +0200

    fix character count

--- packages/character-count-ui/src/CharacterCounter.tsx
+++ packages/character-count-ui/src/CharacterCounter.tsx
@@ -6,10 +6,15 @@ import {TEditor} from '@dreifuss-wysiwyg-editor/common'
 
 const getTextString = (editor: TEditor) => {
   // get all text nodes and append them to each other in one string
-  return [...Node.texts(editor)].reduce((string, nodePair, index) => {
+  return [...Node.texts(editor)].reduce((string, nodePair) => {
     const [textNode] = nodePair
-    if (index === 0) return `${textNode.text}`
-    return `${string} ${textNode.text}`
+    if (string.endsWith(' ') || textNode.text.startsWith(' ')) {
+      return `${string}${textNode.text}`
+    } else if (string === '') {
+      return `${textNode.text}`
+    } else {
+      return `${string} ${textNode.text}`
+    }
   }, '')
 }
 
