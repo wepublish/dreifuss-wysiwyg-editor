d8aeca1f211435300332658c4f8ec7ee39e160a8
bishoy melek <bishoymelekwadie@gmail.com>
2021-10-17 13:57:18 +0200

    fix alignment buttons

--- packages/core/src/Toolbar.tsx
+++ packages/core/src/Toolbar.tsx
@@ -83,10 +83,10 @@ export const ToolbarListButtons = ({editor}: any) => (
 
 export const ToolbarAlignButtons = ({editor}) => (
   <Button editor={editor}>
-    <Button.Element type={ELEMENT_ALIGN_LEFT} icon={<AlignLeftIcon />} />
-    <Button.Element type={ELEMENT_ALIGN_CENTER} icon={<AlignCenterIcon />} />
-    <Button.Element type={ELEMENT_ALIGN_RIGHT} icon={<AlignRightIcon />} />
-    <Button.Element type={ELEMENT_ALIGN_JUSTIFY} icon={<AlignJustifyIcon />} />
+    <Button.Align type={ELEMENT_ALIGN_LEFT} icon={<AlignLeftIcon />} />
+    <Button.Align type={ELEMENT_ALIGN_CENTER} icon={<AlignCenterIcon />} />
+    <Button.Align type={ELEMENT_ALIGN_RIGHT} icon={<AlignRightIcon />} />
+    <Button.Align type={ELEMENT_ALIGN_JUSTIFY} icon={<AlignJustifyIcon />} />
   </Button>
 )
 
