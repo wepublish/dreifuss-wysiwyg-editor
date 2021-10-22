bc2a1296d87f2e41d9ceecc115726095aef3401c
bishoy melek <bishoymelekwadie@gmail.com>
2021-10-22 13:36:42 +0200

    modal enhancements

--- packages/common/src/components/modal/Modal.tsx
+++ packages/common/src/components/modal/Modal.tsx
@@ -28,11 +28,23 @@ export const Modal = ({children, Icon, type}: any) => {
           {type ? <SubMenuIcon type={type} icon={Icon} /> : Icon}
         </div>
         {isMenuOpen && (
-          <div className="modal">
-            <div className="close-container">
-              <div role="presentation" className="close" onClick={() => toggleMenu()}></div>
+          <div
+            className="modal"
+            onClick={() => {
+              // close modal when outside of modal is clicked
+              setIsMenuOpen(false)
+            }}>
+            <div
+              className="modal-content"
+              onClick={e => {
+                // do not close modal if anything inside modal content is clicked
+                e.stopPropagation()
+              }}>
+              <div className="close-container">
+                <div role="presentation" className="close" onClick={() => toggleMenu()}></div>
+              </div>
+              {children}
             </div>
-            {children}
           </div>
         )}
       </div>
